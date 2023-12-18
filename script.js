ab3e6738c88dbf7e5d325a40f049bc1510a9ad67
ryo <ryoneoo97@gmail.com>
2023-12-18 23:42:21 +0630

    new layout

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Loading welcome screen script
 const splash = document.querySelector('.splashScreen');
 const ryo = document.querySelector('.ryo')
+const pageHeading = document.querySelector('.pageHeading')
 
 document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(()=>{
         splash.classList.add('displayNone')
     }, 3000);
-})
-
-document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(()=>{
         ryo.classList.add('moveLeft')
+    }, 2800);
+    setTimeout(()=>{
+        pageHeading.classList.add('topFixed')
     }, 2800);
 })
