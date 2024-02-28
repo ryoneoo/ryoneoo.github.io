5105a757e9eb04c806e391958382638e7b819cea
ryo <ryoneoo97@gmail.com>
2024-02-28 13:49:12 +0700

    Changed website layout

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ document.addEventListener('DOMContentLoaded', (e)=>{
         pageHeading.classList.add('topFixed')
     }, 2800);
 })
+
+function navigationResponse() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
